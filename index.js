4a191c0e59b42f1c47feaa723fba95f6ca480c22
Margareth Borba <mborba1@icloud.com>
2021-10-14 12:41:00 -0400

    updated with RLU cache problems

--- index.js
+++ index.js
@@ -457,3 +457,123 @@ function sameFrequency(num1, num2){
 }
 
 sameFrequency(34, 14)
+
+
+///=======
+// RLU Cache
+
+class LRUCache {
+    constructor(maxSize) {
+          this.cache = {}
+      this.maxSize = maxSize || 1;
+          this.currentSize = 0
+          this.listOfMostRecent = new DoublyLinkedList()
+    }
+  
+    insertKeyValuePair(key, value) {
+      // Write your code here.
+          if(!(key in this.cache)){
+              if(this.currentSize === this.maxSize){
+                  this.evictLeastRecent()
+              }else{
+                  this.currentSize +=1
+              }
+              this.cache[key] = new DoublyLinkedListNode(key, value)
+              
+          }else{
+              this.replaceKey(key, value)
+          }
+          this.updateMostRecent(this.cache[key])
+    }
+    updateMostRecent(node){
+          this.listOfMostRecent.setHeadTo(node)
+      }
+    getValueFromKey(key) {
+      // Write your code here.
+          if(!(key in this.cache)){
+              return null;
+          }else{
+              this.updateMostRecent(this.cache[key])
+              return this.cache[key].value;
+          }
+    }
+  
+    getMostRecentKey() {
+      // Write your code here.
+          if(!this.listOfMostRecent.head) return;
+          return this.listOfMostRecent.head.key
+    }
+      evictLeastRecent() {
+          let keyToRemove = this.listOfMostRecent.tail.key
+          this.listOfMostRecent.removeTail()
+          delete this.cache[keyToRemove]
+      }
+      
+      replaceKey(key, value){
+          if(!(key in this.cache)){
+              throw new error("Key not in cache")
+          }else{
+              this.cache[key].value = value;
+          }
+      }
+  }
+  
+  class DoublyLinkedList{
+      constructor(){
+          this.head = null;
+          this.tail = null;
+      }
+      setHeadTo(node){
+          if (this.head === node){
+              return;
+          }else if(this.head === null){
+              this.head = node;
+              this.tail = node;
+          }else if(this.head === this.tail){
+              this.tail.prev = node;
+              this.head = node;
+              this.head.next = this.tail;
+          }else{
+              if(this.tail === node){
+                  this.removeTail()
+              }
+              node.removeBidings()
+              this.head.prev = node;
+              node.next = this.head;
+              this.head = node;
+          }
+      }
+      removeTail(){
+          if(this.tail === null){
+              return;
+          }
+          if(this.tail === this.head){
+              this.head = null;
+              this.tail = null;
+              return;
+          }
+          this.tail = this.tail.prev;
+          this.tail.next = null;
+      }
+  }
+  class DoublyLinkedListNode{
+      constructor(key, value){
+          this.key = key;
+          this.value = value;
+          this.prev = null;
+          this.next = null;
+      }
+      removeBidings(){
+          if(this.prev !== null){
+              this.prev.next = this.next;
+          }
+          if(this.next !== null){
+              this.next.prev = this.prev;
+          }
+          this.prev = null;
+          this.next = null;
+      }
+  }
+  // Do not edit the line below.
+  exports.LRUCache = LRUCache;
+  
