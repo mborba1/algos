0ca3ee88c8ee82c889541e9d6bb81e6f477df0cb
Margareth Borba <mborba1@icloud.com>
2021-10-25 17:29:30 -0400

    added radixsort algos

--- index.js
+++ index.js
@@ -760,3 +760,45 @@ function threeNumberSort(array, order) {
   }
 
   quickSort([4,6,9,1,2,5])
+
+  //Radix quickSort
+
+  //helper function to get the digit
+
+  function getDigit(num, i){
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+  }
+
+  // getDigit(7323, 0)
+
+  function digitCount(num){
+    if(num === 0) return 1;
+
+    return Math.floor(Math.log10(Math.abs(num))) +1;
+  }
+
+  // digitCount(2138)
+
+  function mostDigits(nums){
+    let maxDigits = 0;
+    for(let i =0; i < nums.length; i++){
+      maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits
+  }
+
+  function radixSort(nums){
+      let maxDigitCount = mostDigits(nums);
+      
+      for(let k =0; k < maxDigitCount; k++){
+          let digitBuckets = Array.from({length: 10}, () => [])
+          for(let i =0; i < nums.length; i++){
+              let digit = getDigit(nums[i],k)
+              digitBuckets[digit].push(nums[i])
+          }
+          nums = [].concat(...digitBuckets)
+      }
+      return nums;
+  }
+
+  radixSort([23, 345, 5467, 12, 2345, 9852])
