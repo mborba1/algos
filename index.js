5ca6fc57d3b0a78435f30f1db3414a600f58c523
Margareth Borba <mborba1@icloud.com>
2021-11-03 11:52:24 -0400

    added more info

--- index.js
+++ index.js
@@ -1026,3 +1026,48 @@ hasBalancedBrackets('text ( is allowed ){rwwrwrrww [] ()}'); // true */
 
 //Time: O(n)
 //Space: O(n)
+
+
+//Stacks
+class Node {
+  constructor(value){
+      this.value = value;
+      this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+  }
+  push(val){
+      let newNode = new Node(val)
+
+      if(!this.first){
+          this.first = newNode;
+          this.last = newNode;
+      }else{
+          let temp = this.first;
+          this.first = newNode;
+          this.first.next = temp;
+
+      }
+      return this.size++
+  }
+  pop(){
+      if(!newNode) return null;
+
+      let temp = this.first
+      if(this.size === this.last){
+          this.last = null;
+      }else{
+          this.first = this.first.next
+     
+          this.size--
+
+      }
+     return temp.value
+  }
+}
