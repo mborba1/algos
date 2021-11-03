fdd71b35b86e8501edaeeaa32f7380f7ab9ef5a4
Margareth Borba <mborba1@icloud.com>
2021-11-03 14:37:21 -0400

    BTS

--- Stack.js
+++ Stack.js
@@ -103,3 +103,62 @@ class Queue{
 //Big O 
 //insertion/ deletion :  O(1)
 //searching/ accessing: O(n)
+
+//Binary Trees/Binary Search Trees
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+        let newNode = new Node(value);
+
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }else {
+            let current = this.root
+            while(true){
+                if(value === current.value) return undefined;
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    }else {
+                        current = current.left;
+                    }
+                }else if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }          
+        }
+    }
+
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+
+//       10
+//   5        13
+// 2   7    11   16
+
+///meetingRooms II - Leetcode 
+//[0,30],[5,10],[15,20]
+//which datastructure = min Heap
