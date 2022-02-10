ef36c6595ad5347909ced6c42bf221f6f34484b5
Margareth Borba <mborba1@icloud.com>
2022-02-10 15:27:50 -0500

    updated linkedlist

--- LinkedList.js
+++ LinkedList.js
@@ -40,10 +40,66 @@ class LinkedList {
         return array;
     }
     insert(idx, value){
-        
-    }
 
-}
+        //check params
+        if(idx===0){
+            this.prepend(value)
+            return this.printList()
+        }
+        if(idx >= this.length){
+            return this.append(value);
+        }
+        //create a new Node object
+        const newNode = {
+            value: value,
+            next: null
+        };
+        //will need to create a methoc
+        const leader = this.traverseToIndex(idx-1)
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList();
+      }
+      traverseToIndex(idx){
+          //check for params
+          let counter =0;
+          let currentNode = this.head;
+          while(counter !== idx){
+              currentNode = currentNode.next;
+              counter++;
+          }
+          return currentNode;
+      }
+      remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.prepend();
+        if(index === this.length -1) return 
+  
+       const leader =  this.traverseToIndex(index-1);
+       const unwantedNode = leader.next;
+       leader.next = unwantedNode.next;
+       this.length--;
+       return this.printList();
+  
+  
+      }
+  
+  }
+  
+  
+  const myLinkedList = new LinkedList(10);
+  
+  myLinkedList.append(5)
+  myLinkedList.append(16)
+  myLinkedList.prepend(1)
+  myLinkedList.insert(2, 99)
+  myLinkedList.insert(20, 88)
+  
+  myLinkedList.printList()
+  myLinkedList.remove(2)
+  // console.log(myLinkedList)
 
 
 const myLinkedList = new LinkedList(10);
