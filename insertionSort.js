cabe5c1d5f77da55fc7725c95a99f281174e0644
Margareth Borba <mborba1@icloud.com>
2022-02-21 18:14:22 -0500

    adding sorting algos problems

---
+++ insertionSort.js
@@ -0,0 +1,24 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+ //insertion sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array){
+  for(let i=0; i< array.length; i++){
+    if(array[i] < array[0]){
+      //move the number to the first postion
+      array.unshift(array.splice(i,1)[0])
+    }else{
+      //find where the number should go
+      for(let j= 1; j< i; j++){
+        if (array[i]> array[j-1] && array[i] < array[j]){
+          //move number to the right spot
+          array.splice(j,0,array.splice(i,1)[0])
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+console.log(numbers)
